2b4e42e4be0514f9f906737a7e136ba7bcbb90c5
Nikita Makarov <quiz.beat@gmail.com>
2023-05-06 22:17:19 +0300

    Move dialogs to a subcollection of a user

--- bot/firestore.py
+++ bot/firestore.py
@@ -1,5 +1,5 @@
 from typing import Optional, Any
-from datetime import datetime
+from datetime import datetime, timezone
 import uuid
 
 import firebase_admin
@@ -16,9 +16,7 @@ class Firestore(database.BotDatabase):
         creds = credentials.Certificate('firebase_service_account.json')
         self.app = firebase_admin.initialize_app(creds)
         self.db = firestore.client()
-
         self.users_ref = self.db.collection("users")
-        self.dialogs_ref = self.db.collection("dialogs")
 
     def check_if_user_exists(self, user_id: int, raise_exception: bool = False) -> bool:
         if self.get_user_snapshot(user_id).exists:
@@ -64,20 +62,20 @@ class Firestore(database.BotDatabase):
         self.check_if_user_exists(user_id, raise_exception=True)
 
         dialog_id = str(uuid.uuid4())
+        chat_mode = self.get_user_attribute(user_id, "current_chat_mode")
+        start_time = datetime.now(timezone.utc)
+        model = self.get_user_attribute(user_id, "current_model")
 
         dialog_dict = {
-            "user_id": user_id,
-            "chat_mode": self.get_user_attribute(user_id, "current_chat_mode"),
-            "start_time": datetime.now(),
-            "model": self.get_user_attribute(user_id, "current_model"),
+            "chat_mode": chat_mode,
+            "start_time": start_time,
+            "model": model,
             "messages": []
         }
 
-        # TODO: Store dialogs as a subcollection inside a user
-
-        # add new dialog
-        new_dialog_ref = self.dialogs_ref.document(f"{dialog_id}")
-        new_dialog_ref.set(dialog_dict)
+        dialogs_collection = self.get_dialogs_collection(user_id)
+        dialog_ref = dialogs_collection.document(f"{dialog_id}")
+        dialog_ref.set(dialog_dict)
 
         # update user's current dialog
         self.set_current_dialog_id(user_id, dialog_id)
@@ -104,7 +102,9 @@ class Firestore(database.BotDatabase):
         if dialog_id is None:
             dialog_id = self.get_current_dialog_id(user_id)
 
-        dialog_dict = self.get_dialog_snapshot(dialog_id).to_dict()
+        dialogs_collection = self.get_dialogs_collection(user_id)
+        dialog_ref = dialogs_collection.document(dialog_id)
+        dialog_dict = dialog_ref.get().to_dict()
         
         return dialog_dict["messages"]
 
@@ -114,13 +114,9 @@ class Firestore(database.BotDatabase):
         if dialog_id is None:
             dialog_id = self.get_current_dialog_id(user_id)
 
-        # Maybe should check the user_id
-        self.get_dialog_ref(dialog_id).update({"messages": dialog_messages})
-
-        # self.dialog_collection.update_one(
-        #     {"_id": dialog_id, "user_id": user_id},
-        #     {"$set": {"messages": dialog_messages}}
-        # )
+        dialogs_collection = self.get_dialogs_collection(user_id)
+        dialog_ref = dialogs_collection.document(dialog_id)
+        dialog_ref.update({"messages": dialog_messages})
 
     def get_last_interaction(self, user_id: int) -> datetime:
         google_last_interaction = self.get_user_attribute(user_id, "last_interaction")
@@ -150,11 +146,8 @@ class Firestore(database.BotDatabase):
     def set_n_used_tokens(self, user_id: int, new_value):
         self.set_user_attribute(user_id, "n_used_tokens", new_value)
 
-    def get_dialog_ref(self, dialog_id: str):
-        return self.dialogs_ref.document(dialog_id)
-    
-    def get_dialog_snapshot(self, dialog_id: str):
-        return self.get_dialog_ref(dialog_id).get()
+    def get_dialogs_collection(self, user_id: int):
+        return self.get_user_ref(user_id).collection("dialogs")
 
     def get_user_attribute(self, user_id: int, key: str):
         self.check_if_user_exists(user_id, raise_exception=True)
